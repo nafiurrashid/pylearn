6ca17692d0b62dafacab6089ec018f207a89005e
nafiurrashid <nafiurrashid@gmail.com>
2021-06-15 16:29:48 +0600

    first commit

---
+++ project2/calc/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path #import path from project urls.py
+from . import views #import views from all 
+
+urlpatterns =[
+    path('',views.home, name='home')
+]
+#mutiple urls to mapping will be withing this array
+#IN views.home home means there will be a home function
